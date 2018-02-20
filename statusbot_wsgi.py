02043fd386723388f36b844e1a88e6feb2654077
Toby Jones <psytj1@nottingham.ac.uk>
2018-02-20 22:41:57 +0000

    Added some files to facilitate running in wsgi, fixed google fonts serving over https & removed a broken reference to a JS file

---
+++ statusbot_wsgi.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, make_response, request
+from pymongo import MongoClient
+import statusbot_settings as settings
+import datetime
+import uuid
+
+webapp = Flask(__name__)
+dbconn = MongoClient(settings.database_host, settings.database_port, connect=False)
+
+database = dbconn[settings.database_name]
+dbcollection = database[settings.database_collection]
+
+@webapp.route("/")
+def display_homepage(issue_list=None, app_name=None, app_slogan=None):
+	return render_template("home.html", issue_list=dbcollection, app_name=settings.app_name, app_slogan=settings.app_slogan)
+
+@webapp.route("/issues")
+def display_issues(issue_list=None, app_name=None):
+	return render_template("issues.html", issue_list=dbcollection, app_name=settings.app_name)
